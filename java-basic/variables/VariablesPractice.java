a179a6c77a21ef48d89af6ca7adc6c90511d5ad8
SnWalker <lf109103@163.com>
2025-12-02 23:31:28 +0800

    Java variables types

---
+++ java-basic/variables/VariablesPractice.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        //byte a = 128;
+        byte a = (byte)128;
+        System.out.println(a);
+    }
+}
