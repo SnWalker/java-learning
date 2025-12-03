f4e2eb0138640225091daef3194709c0727cbb58
SnWalker <lf109103@163.com>
2025-12-03 16:16:54 +0800

    Java operators and expressions

---
+++ java-basic/variables/OperatorAndExpression.java
@@ -0,0 +1,30 @@
+package variables;
+
+public class OperatorAndExpression {
+    public static void main(String[] args) {
+//        byte b1 = 100;
+//        byte b2 = 10;
+//        byte b3 = b1 + b2;
+//        short s = b1 + b2;
+//        System.out.println(s);
+
+//        int i = 0;
+//        if (false && (++i > 0)) {
+//            // 空
+//        }
+//        System.out.println(i);
+//        i = 0;
+//        if (false & (++i > 0)) {
+//            // 空
+//        }
+//        System.out.println(i);
+
+//        System.out.println(255 & 1);
+
+//        System.out.println(true ^ false);
+//
+//        System.out.println(!!true);
+
+        int a = 1 > 2 ? 3 : 4;
+    }
+}
--- java-basic/variables/VariablesPractice.java
+++ java-basic/variables/VariablesPractice.java
@@ -1,9 +1,22 @@
 package variables;
-
+import java.util.Scanner;
 public class VariablesPractice {
     public static void main(String[] args) {
         //byte a = 128;
-        byte a = (byte)128;
+        //byte a = (byte)128;
+
+        //long a = 128L;
+        //long a = 2147483648; // 整数字面量默认是int类型，超出int范围报编译错误
+        //long a = 2147483648L;
+
+        //double a = 3.14;
+        //float a = 3.14; // 小数的默认类型是double，转换为float可能有损失，报编译错误
+        //float a = (float)3.14; // 强制类型转换
+        float a = 3.14F;
+
+        //String name = "xiao wang";
+        //System.out.println(name);
+
         System.out.println(a);
     }
 }
